8cc1254b5d1da25f06b0254625c8af17f03767b2
indigoparadox <1065446+indigoparadox@users.noreply.github.com>
2014-03-21 15:33:06 -0400

    Moved sync/storage tangles to their own class.

--- matchmouse/__init__.py
+++ matchmouse/__init__.py
@@ -17,5 +17,5 @@ You should have received a copy of the GNU General Public License along
 with MatchMouse.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
-__all__ = ['browser','storage','syncher']
+__all__ = ['browser','storage','syncher','syncstorage']
 
