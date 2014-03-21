473f5b60fa35f87aeb3259c20380e8737c35c73a
indigoparadox <1065446+indigoparadox@users.noreply.github.com>
2014-03-21 11:48:41 -0400

    Basic sync protocol established.

--- matchmouse/syncher.py
+++ matchmouse/syncher.py
@@ -19,30 +19,109 @@ with MatchMouse.  If not, see <http://www.gnu.org/licenses/>.
 
 import base64
 import requests
+import hashlib
+import json
+import logging
+import hmac
+from M2Crypto.EVP import Cipher
+
+SYNC_API = '1.0'
 
 class MatchMouseSyncher():
 
+   logger = None
    browser = None
    server = ''
    username = ''
    password = ''
-   key = ''
+   local_key = ''
+   #node = ''
+   priv_key = ''
+   priv_hmac = ''
 
    def __init__( self, browser, server='', username='', password='', key='' ):
+
+      # Setup member fields.
+      self.logger = logging.getLogger( 'matchmouse.syncher' )
       self.browser = browser
       self.server = server
-      self.username = username
+      self.userhash = self._encode_username( username )
       self.password = password
-      self.key = key
+      #self.node = self._request_node().rstrip( '/' )
+      self.local_key = self._digest_key( key )
 
-   def _encode_username( username ):
+      #self.logger.debug( 'Node set to: {}'.format( self.node ) )
+
+      default_key = self._decrypt_key( self.local_key )
+      self.priv_key = default_key[0].decode( 'base64' )
+      self.priv_hmac = default_key[1].decode( 'base64' )
+
+   def _digest_key( self, key ):
+
+      # Strip out/replace invalid characters.
+      normalized_key = \
+         key.replace( '-', '' ).replace( '8', 'l' ).replace( '9', 'o' ).upper()
+      padding = (8-len(normalized_key) % 8) % 8
+      normalized_key += '=' * padding
+      normalized_key = base64.b32decode( normalized_key )
+
+      # Return key digest.
+      return hmac.new(
+         normalized_key,
+         '{}{}\x01'.format( 'Sync-AES_256_CBC-HMAC256', self.userhash ),
+         hashlib.sha256
+      ).digest()
+
+   def _decrypt( self, payload, key ):
+
+      ''' Decrypt payload fetched from server. '''
+
+      ciphertext = payload['ciphertext'].decode( 'base64' )
+      iv = payload['IV'].decode( 'base64' )
+
+      # Perform the actual decryption.
+      cipher = Cipher( alg='aes_256_cbc', key=key, iv=iv, op=0 )
+      v = cipher.update( ciphertext )
+      v = v + cipher.final()
+      del cipher
+
+      return json.loads( v )
+      
+   def _decrypt_key( self, key ):
+
+      ''' Decrypt server-side key with our local key. '''
+
+      data = self._request_path( 'storage/crypto/keys' )
+      payload = json.loads( data['payload'] )
+      return self._decrypt( payload, key )['default']
+
+   def _encode_username( self, username ):
       return base64.b32encode( hashlib.sha1( username ).digest() ).lower()
 
    def _request_node( self ):
-      url = '{}/user/1/{}/node/weave'.format( self.server, self.username )
-      r = requests.get( url, auth=(self.username, self.password) )
-      return r.read()
+
+      # TODO: What is the node even for? Just returns "1" all the time.
+
+      url = '{}/user/1.0/{}'.format( self.server, self.userhash )
+      r = requests.get( url, auth=(self.userhash, self.password) )
+      return r.text
+
+   def _request_path( self, path ):
+      url = '/'.join( (self.server, SYNC_API, self.userhash, path ) )
+      r = requests.get( url, auth=(self.userhash, self.password) )
+      return json.loads( r.text )
+
+   def request_bookmark( self, id ):
+      data = self._request_path( 'storage/bookmarks/{}'.format( id ) )
+      payload = json.loads( data['payload'] )
+      return self._decrypt( payload, self.priv_key )
+
+   def list_remote_bookmarks( self ):
+      return self._request_path( 'storage/bookmarks' )
 
    def sync( self ):
-      pass
+      for bookmark_id_iter in self.list_remote_bookmarks():
+         # TODO: Check if bookmark exists in storage and add/remove it
+         #       accordingly.
+         pass
 
