ad20d9b0b8a3dd6be5b9128256990653de1e0fc7
indigoparadox <1065446+indigoparadox@users.noreply.github.com>
2014-03-25 10:33:58 -0400

    Updated README/__init__.py to match current project state.

--- matchmouse/__init__.py
+++ matchmouse/__init__.py
@@ -17,5 +17,7 @@ You should have received a copy of the GNU General Public License along
 with MatchMouse.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
-__all__ = ['browser','storage','syncher','syncstorage']
+__all__ = [
+   'browser','downloads','options''storage','syncher','syncstorage','tab'
+]
 
